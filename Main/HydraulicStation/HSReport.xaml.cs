262827c540ae4a97792adad557f7af40a1419b34
lyn5284767 <13770945149@163.com>
2020-09-15 17:57:55 +0800

    液压站报表和列表页面

---
+++ Main/HydraulicStation/HSReport.xaml.cs
@@ -0,0 +1,240 @@
+﻿using COM.Common;
+using DatabaseLib;
+using LiveCharts;
+using LiveCharts.Wpf;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Main.HydraulicStation
+{
+    /// <summary>
+    /// HSReport.xaml 的交互逻辑
+    /// </summary>
+    public partial class HSReport : UserControl
+    {
+        private static HSReport _instance = null;
+        private static readonly object syncRoot = new object();
+        public static HSReport Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new HSReport();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+        /// <summary>
+        /// 系统压力值
+        /// </summary>
+        public SeriesCollection SystemPressSeries { get; set; }
+        /// <summary>
+        /// 系统压力时间
+        /// </summary>
+        public List<string> SystemPressLabels { get; set; }
+        /// <summary>
+        /// 油温
+        /// </summary>
+        public SeriesCollection OilTemSeries { get; set; }
+        /// <summary>
+        /// 油温时间
+        /// </summary>
+        public List<string> OilTemLabels { get; set; }
+        /// <summary>
+        /// 液压值
+        /// </summary>
+        public SeriesCollection OilLevelSeries { get; set; }
+        /// <summary>
+        /// 液压时间
+        /// </summary>
+        public List<string> OilLevelLabels { get; set; }
+
+        public HSReport()
+        {
+            InitializeComponent();
+            VariableBinding();
+            this.Loaded += HSReport_Loaded;
+        }
+        /// <summary>
+        /// 页面加载事件
+        /// </summary>
+        private void HSReport_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.tbReportTime.Text = "报表生成时间:" + DateTime.Now.ToString("yyyy-MM-dd HH:mm");
+            BtnDay_Click(null, null);
+        }
+
+        private void VariableBinding()
+        {
+            this.tbMainOneTime.SetBinding(TextBlock.TextProperty, new Binding("IntTag") { Source = GlobalData.Instance.da["M1RunTime"], Mode = BindingMode.OneWay, Converter = new DivideTenConverter() });
+            this.tbMainTwoTime.SetBinding(TextBlock.TextProperty, new Binding("IntTag") { Source = GlobalData.Instance.da["M2RunTime"], Mode = BindingMode.OneWay, Converter = new DivideTenConverter() });
+            this.tbConstantVoltage.SetBinding(TextBlock.TextProperty, new Binding("IntTag") { Source = GlobalData.Instance.da["CRunTime"], Mode = BindingMode.OneWay, Converter = new DivideTenConverter() });
+            this.tbDissipateHeat.SetBinding(TextBlock.TextProperty, new Binding("IntTag") { Source = GlobalData.Instance.da["DRunTime"], Mode = BindingMode.OneWay, Converter = new DivideTenConverter() });
+        }
+        /// <summary>
+        /// 预览报表
+        /// </summary>
+        private void btn_PreviewReport(object sender, RoutedEventArgs e)
+        {
+            string filePath = System.Environment.CurrentDirectory + "\\HSReport";
+            if (!File.Exists(filePath + @"\image.jpg"))
+            {
+                MessageBox.Show("请先生成报表");
+            }
+            Process.Start(filePath + @"\image.jpg");
+        }
+        /// <summary>
+        /// 生成报表
+        /// </summary>
+        private void btn_GenerateReport(object sender, RoutedEventArgs e)
+        {
+            string filePath = System.Environment.CurrentDirectory + "\\HSReport";
+            if (!Directory.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+            // 页面保存为图片
+            FileStream fs = new FileStream(filePath +@"\image.jpg", FileMode.Create);
+            RenderTargetBitmap bmp = new RenderTargetBitmap((int)this.gdMain.ActualWidth, (int)this.gdMain.ActualHeight, 0, 0, PixelFormats.Pbgra32);
+            bmp.Render(this.gdMain);
+            BitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(bmp));
+            encoder.Save(fs);
+            fs.Close();
+            fs.Dispose();
+
+            PdfDocument doc = new PdfDocument();
+
+            doc.Pages.Add(new PdfPage());
+            doc.Pages[0].Size = PageSize.QuadDemy;
+            XGraphics xgr = XGraphics.FromPdfPage(doc.Pages[0]);
+            XImage img = XImage.FromFile(filePath + @"\image.jpg");
+            xgr.DrawImage(img, 0, 0, img.Size.Width, img.Size.Height);
+            doc.Save(filePath +"\\"+ DateTime.Now.ToString("yyyyMMdd") + ".pdf");
+            doc.Close();
+        }
+
+        /// <summary>
+        /// 查询一天内
+        /// </summary>
+        private void BtnDay_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var bc = new BrushConverter();
+                this.btnDay.Foreground = (Brush)bc.ConvertFrom("#FFFFFF");
+                this.btnDay.Background = (Brush)bc.ConvertFrom("#326CF3");
+                this.btnMonth.Foreground = (Brush)bc.ConvertFrom("#525252");
+                this.btnMonth.Background = (Brush)bc.ConvertFrom("#FFFFFF");
+                DrawSystemPress(DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd HH:mm"));
+                DrawOilTem(DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd HH:mm"));
+                DrawOilLevel(DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd HH:mm"));
+                DataContext = null;
+                DataContext = this;
+            }
+            catch (Exception ex)
+            {
+                Log.Log4Net.AddLog(ex.StackTrace, Log.InfoLevel.ERROR);
+            }
+        }
+        /// <summary>
+        /// 绘制压力曲线
+        /// </summary>
+        private void DrawSystemPress(string Date)
+        {
+            LineSeries systemPressLine = new LineSeries();
+            systemPressLine.LineSmoothness = 1;
+            systemPressLine.PointGeometry = null;
+            SystemPressLabels = new List<string>();
+            systemPressLine.Values = new ChartValues<double>();
+            SystemPressSeries = new SeriesCollection { };
+            SystemPressSeries.Add(systemPressLine);
+            string sql = string.Format("Select * from DateBaseReport Where Type = '{0}' and CreateTime>'{1}'", (int)SaveType.HS_Self_SystemPress, Date);
+            List<DateBaseReport> systemPressList = DataHelper.Instance.ExecuteList<DateBaseReport>(sql);
+            foreach (DateBaseReport dateBaseReport in systemPressList)
+            {
+                SystemPressLabels.Add(dateBaseReport.CreateTime.ToString());
+                SystemPressSeries[0].Values.Add(double.Parse(dateBaseReport.Value));
+            }
+        }
+        /// <summary>
+        /// 绘制油温曲线
+        /// </summary>
+        private void DrawOilTem(string Date)
+        {
+            LineSeries oilTemLine = new LineSeries();
+            oilTemLine.LineSmoothness = 0;
+            oilTemLine.PointGeometry = null;
+            OilTemLabels = new List<string>();
+            oilTemLine.Values = new ChartValues<double>();
+            OilTemSeries = new SeriesCollection { };
+            OilTemSeries.Add(oilTemLine);
+            string sql = string.Format("Select * from DateBaseReport Where Type = '{0}' and CreateTime>'{1}'", (int)SaveType.HS_Self_OilTmp, Date);
+            List<DateBaseReport> OilTemList = DataHelper.Instance.ExecuteList<DateBaseReport>(sql);
+            foreach (DateBaseReport dateBaseReport in OilTemList)
+            {
+                OilTemLabels.Add(dateBaseReport.CreateTime.ToString());
+                OilTemSeries[0].Values.Add(double.Parse(dateBaseReport.Value));
+            }
+        }
+        /// <summary>
+        /// 绘制液位曲线
+        /// </summary>
+        private void DrawOilLevel(string Date)
+        {
+            LineSeries oilLevelLine = new LineSeries();
+            oilLevelLine.LineSmoothness = 0;
+            oilLevelLine.PointGeometry = null;
+            OilLevelLabels = new List<string>();
+            oilLevelLine.Values = new ChartValues<double>();
+            OilLevelSeries = new SeriesCollection { };
+            OilLevelSeries.Add(oilLevelLine);
+            string sql = string.Format("Select * from DateBaseReport Where Type = '{0}' and CreateTime>'{1}'", (int)SaveType.HS_Self_OilLevel, Date);
+            List<DateBaseReport> OilLevelList = DataHelper.Instance.ExecuteList<DateBaseReport>(sql);
+            foreach (DateBaseReport dateBaseReport in OilLevelList)
+            {
+                OilLevelLabels.Add(dateBaseReport.CreateTime.ToString());
+                OilLevelSeries[0].Values.Add(double.Parse(dateBaseReport.Value));
+            }
+        }
+        /// <summary>
+        /// 查询一月内
+        /// </summary>
+        private void BtnMounth_Click(object sender, RoutedEventArgs e)
+        {
+            var bc = new BrushConverter();
+            this.btnDay.Foreground = (Brush)bc.ConvertFrom("#525252");
+            this.btnDay.Background = (Brush)bc.ConvertFrom("#FFFFFF");
+            this.btnMonth.Foreground = (Brush)bc.ConvertFrom("#FFFFFF");
+            this.btnMonth.Background = (Brush)bc.ConvertFrom("#326CF3");
+            DrawSystemPress(DateTime.Now.AddDays(-30).ToString("yyyy-MM-dd HH:mm"));
+            DrawOilTem(DateTime.Now.AddDays(-30).ToString("yyyy-MM-dd HH:mm"));
+            DrawOilLevel(DateTime.Now.AddDays(-30).ToString("yyyy-MM-dd HH:mm"));
+            DataContext = null;
+            DataContext = this;
+        }
+    }
+}
---
+++ Main/SIR/SIRSelfRecord.xaml.cs
@@ -0,0 +1,154 @@
+﻿using COM.Common;
+using DatabaseLib;
+using Main.SecondFloor;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Main.SIR
+{
+    /// <summary>
+    /// SIRSelfRecord.xaml 的交互逻辑
+    /// </summary>
+    public partial class SIRSelfRecord : UserControl
+    {
+        private static SIRSelfRecord _instance = null;
+        private static readonly object syncRoot = new object();
+
+        public static SIRSelfRecord Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new SIRSelfRecord();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+        Dictionary<int, string> dicNumToValue = new Dictionary<int, string>();
+        BackgroundWorker bgMeet;
+        public SIRSelfRecord()
+        {
+            InitializeComponent();
+            Init();
+            this.Loaded += SIRSelfRecord_Loaded;
+        }
+
+        private void SIRSelfRecord_Loaded(object sender, RoutedEventArgs e)
+        {
+            QueryRecord();
+            this.beginTime.SelectedDate = DateTime.Now.AddDays(-1);
+            this.endTime.SelectedDate = DateTime.Now.AddDays(1);
+        }
+
+        /// <summary>
+        /// 初始化查询类型
+        /// </summary>
+        public void Init()
+        {
+            dicNumToValue.Add(0, "全部");
+            dicNumToValue.Add(1, "系统压力");
+            dicNumToValue.Add(2, "油温");
+            dicNumToValue.Add(3, "液压");
+
+            this.cbTypeSelect.ItemsSource = dicNumToValue;
+            this.cbTypeSelect.SelectedValuePath = "Key";
+            this.cbTypeSelect.DisplayMemberPath = "Value";
+            this.cbTypeSelect.SelectedIndex = 0;
+        }
+
+
+        private void QueryRecord()
+        {
+            bgMeet = new BackgroundWorker();
+            //能否报告进度更新
+            bgMeet.WorkerReportsProgress = true;
+            //要执行的后台任务
+            bgMeet.DoWork += new DoWorkEventHandler(bgMeet_DoWork);
+            //进度报告方法
+            bgMeet.ProgressChanged += new ProgressChangedEventHandler(bgMeet_ProgressChanged);
+            //后台任务执行完成时调用的方法
+            bgMeet.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgMeet_RunWorkerCompleted);
+            bgMeet.RunWorkerAsync(); //任务启动
+        }
+
+        public delegate void ShowRecord();//定义委托
+
+        private void BindingRecord()
+        {
+            string sql = string.Format("SELECT  * FROM DateBaseReport {0} Order by OID DESC LIMIT 0,1000", cond);
+            List<DateBaseReport> recordList = DataHelper.Instance.ExecuteList<DateBaseReport>(sql);
+            List<ShowDate> showList = new List<ShowDate>();
+            if (recordList != null)
+            {
+                foreach (DateBaseReport date in recordList)
+                {
+                    ShowDate showDate = new ShowDate();
+                    showDate.OID = date.ID;
+                    showDate.Content = date.Name;
+                    showDate.Value = date.Value;
+                    showDate.TimeStamp = date.CreateTime.ToString("yyyy-MM-dd HH:mm:ss");
+                    showList.Add(showDate);
+                }
+            }
+            this.lvRecord.ItemsSource = showList;
+        }
+
+        //执行任务
+        void bgMeet_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //开始播放等待动画
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                this.projectLoad.Visibility = System.Windows.Visibility.Visible;
+            }));
+            Dispatcher.BeginInvoke(DispatcherPriority.Normal, new ShowRecord(BindingRecord));
+        }
+        //报告任务进度
+        void bgMeet_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //this.Dispatcher.Invoke(new Action(() =>
+            //{
+            //    this.lab_pro.Content = e.ProgressPercentage + "%";
+            //}));
+        }
+        //任务执行完成后更新状态
+        void bgMeet_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.projectLoad.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        string cond = "";
+
+        private void Button_MouseDown(object sender, RoutedEventArgs e)
+        {
+            string tagID = this.cbTypeSelect.SelectedValue.ToString();
+            string bTime = this.beginTime.SelectedDate.Value.ToString("yyyy-MM-dd HH:mm:ss");
+            string eTime = this.endTime.SelectedDate.Value.ToString("yyyy-MM-dd HH:mm:ss");
+            string condition = string.Empty;
+            if (tagID != "0") condition = string.Format("Where Type = '{0}' and CreateTime>'{1}' and CreateTime<'{2}'", tagID, bTime, eTime);
+            else condition = string.Format("Where CreateTime>'{0}' and CreateTime<'{1}'", bTime, eTime);
+            cond = condition;
+            QueryRecord();
+        }
+    }
+}
